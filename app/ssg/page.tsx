9ae916cfb157624165fae676bdb11bde0376f5c8
jun094 <jun094@naver.com>
2023-05-24 00:28:57 +0900

    feat: ssg 생성

--- app/ssg/page.tsx
+++ app/ssg/page.tsx
@@ -1,5 +1,29 @@
-function Ssg() {
-  return <div>ssg 페이지입니다.</div>;
+type TodoType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
+const fetchTodo = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/todos`, { cache: 'force-cache' });
+  const todo: TodoType[] = await res.json();
+  return todo;
+};
+
+async function Ssg() {
+  const todo = await fetchTodo();
+
+  return (
+    <div>
+      ssg 페이지입니다.
+      <ul>
+        {todo.map(({ id, title }: any) => (
+          <h4 key={id}>{title}</h4>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Ssg;
